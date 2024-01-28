6dfb3a300d69b3513c8e94a829e9a20b36b4c423
Margarita Ivantsova <148205607+IIMargo@users.noreply.github.com>
2024-01-28 18:23:28 +0300

    ready

---
+++ src/main/java/web/services/UsersService.java
@@ -0,0 +1,47 @@
+package web.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import web.models.User;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Repository
+@Transactional(readOnly = true)
+public class UsersService {
+
+    @PersistenceContext
+    private final EntityManager entityManager;
+
+    @Autowired
+    public UsersService(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+
+    public List<User> findAll() {
+        return entityManager.createQuery("from User", User.class).getResultList();
+    }
+
+    public User findOne(int id) {
+        return entityManager.find(User.class, id);
+    }
+
+    @Transactional
+    public void save(User user) {
+        entityManager.persist(user);
+    }
+
+    @Transactional
+    public void update(int id, User updatedUser) {
+        entityManager.merge(updatedUser);
+    }
+
+    @Transactional
+    public void delete(int id) {
+        entityManager.remove(entityManager.find(User.class, id));
+    }
+}
